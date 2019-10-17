78e66557027b7d3b11b655f75c4ee6b083e87f90
Prakash Badhe <prakash.badhe@agilesoft.in>
2019-10-17 06:38:18 +0530

    first commit

---
+++ src/com/example/simple/User.java
@@ -0,0 +1,17 @@
+package com.example.simple;
+
+public class User {
+
+	public void update()
+	{
+		if(true)
+			System.out.println("Test True");
+		else
+			System.out.println("Test False");
+		}
+	
+	private String getData()
+	{
+		return "Data";
+	}
+}
